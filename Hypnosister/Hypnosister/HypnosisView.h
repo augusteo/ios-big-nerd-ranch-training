03ea61146ad64514d5559d6d4dce6cfaeccb1472
Ryan Blunden <rblunden@linkedin.com>
2013-01-14 15:20:34 -0500

    The best graphics you've ever seen

---
+++ Hypnosister/Hypnosister/HypnosisView.h
@@ -0,0 +1,12 @@
+//
+// Created by rblunden on 1/14/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface HypnosisView : UIView
+@end
