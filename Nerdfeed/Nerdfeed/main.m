b88d2903d3b67ba63e2bc023caa2cda059976b90
Ryan Blunden <rblunden@linkedin.com>
2013-01-20 22:31:14 -0800

    Added NerdFeed project files

---
+++ Nerdfeed/Nerdfeed/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Ryan Blunden on 01/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+
+}
